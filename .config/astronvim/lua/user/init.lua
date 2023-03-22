e384697532891774d872c3e7eab18c95fefe2d0a
Jareng <sanka59@gmail.com>
2023-03-22 20:26:36 +0100

    update astronvim config

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -25,13 +25,13 @@ return {
     virtual_text = true,
     underline = true,
   },
-
+ 
   lsp = {
     -- customize lsp formatting options
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = false, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
--- .config/astronvim/lua/user/plugins/treesitter.lua
+++ .config/astronvim/lua/user/plugins/treesitter.lua
@@ -11,6 +11,7 @@ return {
       "rasi",
       "gitcommit",
       "gitignore",
+      "toml",
     },
   },
 }
