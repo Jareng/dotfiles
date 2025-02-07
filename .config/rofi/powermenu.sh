511e9286ee03149b0e0402ce2d08fcaa0743c89b
Jareng <sanka59@gmail.com>
2025-02-07 20:02:40 +0100

    update rofi config

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -1,26 +1,7 @@
 #!/usr/bin/env bash
 
 rofi \
-	-show p \
-	-modi p:'~/.config/rofi/scripts/rofi-power-menu' \
-	-theme-str '@import "powermenu.rasi"'
-
-# chosen="$(printf "  Power Off\n  Restart\n  Logout" | rofi -dmenu -i -theme-str '@import "powermenu.rasi"')"
-# poweroff="systemctl poweroff"
-# reboot="systemctl reboot"
-# logout="$LOGOUT_CMD"
-#
-# case "$chosen" in
-# "  Power Off")
-# 	"$poweroff"
-# 	;;
-# "  Restart")
-# 	"$reboot"
-# 	;;
-# "  Logout")
-# 	"$logout"
-# 	;;
-# *)
-# 	exit 1
-# 	;;
-# esac
+  -show p \
+  -modi p:'rofi-power-menu --symbols-font "Symbols Nerd Font Mono"' \
+  -font "Hack Mono NF 16" \
+  -theme-str '@import "powermenu.rasi"'
